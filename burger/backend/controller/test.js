9881c40e05ab1af95ee157a373b3d96a598ce2c4
Leo Xie <ziyangxie01@gmail.com>
2024-04-13 16:58:24 -0500

    Backend Dev Test

--- burger/backend/controller/test.js
+++ burger/backend/controller/test.js
@@ -12,14 +12,38 @@ router.get('/', async (req, res) => {
     });
 });
 
-router.get('/user', async (req, res) => {
-    try {
-        const data = await db.query('SELECT * FROM User');
-        res.json({ message: 'Data received', data: data });
-    } catch (err) {
-        res.status(500).json({ message: 'Internal server error' });
-    }
+router.get('/table', async (req, res) => {
+    db.getConnection((err, connection) => {
+        if (err) {
+            res.status(500).json({ message: 'Internal server error' });
+        } else {
+            connection.query('SHOW TABLES', (err, rows) => {
+                if (err) {
+                    res.status(500).json({ message: 'Internal server error' });
+                } else {
+                    res.json(rows);
+                }
+            });
+        }
+    });
 });
+
+router.get('/user', async (req, res) => {
+    db.getConnection((err, connection) => {
+        if (err) {
+            res.status(500).json({ message: 'Internal server error' });
+        } else {
+            connection.query('SELECT * FROM User', (err, rows) => {
+                if (err) {
+                    res.status(500).json({ message: 'Internal server error' });
+                } else {
+                    res.json(rows);
+                }
+            });
+        }
+    });
+});
+
 
 
 
