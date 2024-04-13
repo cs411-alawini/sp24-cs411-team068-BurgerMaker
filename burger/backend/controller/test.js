3b4527c9f1686453642e3a5cecbc5d3f03ae655f
Leo Xie <ziyangxie01@gmail.com>
2024-04-13 16:53:03 -0500

    Backend Dev Test code

--- burger/backend/controller/test.js
+++ burger/backend/controller/test.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const db = require('../db/connection');
 const router = express.Router();
 
-router.get('/simple', async (req, res) => {
-    res.json({ message: 'Data received', yourData: req.body });
+router.get('/', async (req, res) => {
+    db.getConnection((err, connection) => {
+        if (err) {
+            res.status(500).json({ message: 'Internal server error' });
+        } else {
+            res.json({ message: 'Connected to database' });
+        }
+    });
 });
+
+router.get('/user', async (req, res) => {
+    try {
+        const data = await db.query('SELECT * FROM User');
+        res.json({ message: 'Data received', data: data });
+    } catch (err) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+
 
 
 
